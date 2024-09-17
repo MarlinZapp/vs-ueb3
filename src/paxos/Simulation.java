87f1b4bd7220cba2d1443d309f2e1e043665262f
Marlin Zapp <s4mazapp@uni-trier.de>
2024-09-17 12:11:17 +0200

    added parameter for number of fishes to Simulation.java

--- src/paxos/Simulation.java
+++ src/paxos/Simulation.java
@@ -3,12 +3,13 @@ package paxos;
 import org.oxoo2a.sim4da.Simulator;
 
 public class Simulation {
+    public static int NUMBER_OF_FISHES = 20;
     public static double DANGER_CHANCE = 0.01;
     public static double FOOD_OCCURENCE = 0.05;
 
     public static void main(String[] args) {
         Simulator simulator = Simulator.getInstance();
-        Swarm swarm = new Swarm(20);
+        Swarm swarm = new Swarm(NUMBER_OF_FISHES);
         simulator.simulate();
         new Thread(() -> {
             try {
